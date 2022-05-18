e7e4611d81a3bc0f4e19536fcf3fe8c2aa10c8ba
Omar Wathaifi <omar.ghassan@softylus.com>
2022-05-19 00:17:17 +0300

    edit code

--- lib/components/constant.dart
+++ lib/components/constant.dart
@@ -1,33 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:vitality/components/color.dart';
+import 'package:vitality/components/fonts.dart';
 
-void showDoneSnackBar(BuildContext context,String text) {
-    final snackBar = SnackBar(
-      content: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          const Icon(Icons.done_outline, size: 20,color: Colors.green,),
-          const SizedBox(width: 16),
-          Expanded(
-            child: Text(
-              text,
-              style: const TextStyle(fontSize: 15,color: Colors.green),
-            ),
-          ),
-        ],
-      ),
-      backgroundColor: Colors.black45,
-      duration: const Duration(seconds: 3),
-      behavior: SnackBarBehavior.floating,
-    );
-    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-  }
-
-void showErrorSnackBar(BuildContext context,var text) {
+void showDoneSnackBar(BuildContext context, String text) {
   final snackBar = SnackBar(
     content: Row(
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        const Icon(Icons.error_outline, size: 20,color: Colors.black,),
+        const Icon(
+          Icons.done_outline,
+          size: 20,
+          color: Colors.green,
+        ),
+        const SizedBox(width: 16),
+        Expanded(
+          child: Text(
+            text,
+            style: const TextStyle(fontSize: 15, color: Colors.green),
+          ),
+        ),
+      ],
+    ),
+    backgroundColor: Colors.black45,
+    duration: const Duration(seconds: 3),
+    behavior: SnackBarBehavior.floating,
+  );
+  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+}
+
+void showErrorSnackBar(BuildContext context, var text) {
+  final snackBar = SnackBar(
+    content: Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        const Icon(
+          Icons.error_outline,
+          size: 20,
+          color: Colors.black,
+        ),
         const SizedBox(width: 16),
         Expanded(
           child: Text(
@@ -42,4 +52,41 @@ void showErrorSnackBar(BuildContext context,var text) {
     behavior: SnackBarBehavior.floating,
   );
   ScaffoldMessenger.of(context).showSnackBar(snackBar);
+}
+
+void showAlertDialog(BuildContext context) {
+  showDialog<String>(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(
+          'congratulations',
+          style: AppFonts.tajawal16GreenW600,
+          textAlign: TextAlign.center,
+        ),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              Text(
+                'You got a 4 week subscription for free',
+                style: AppFonts.tajawal14BlackW400,
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          Center(
+            child: TextButton(
+              child: const Text('Okay'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ),
+        ],
+      );
+    },
+  );
 }
